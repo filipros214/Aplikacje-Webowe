9f0467c7bc6b99b527568325f4a41b3218f439b8
Filip <millerfilip214@gmail.com>
2026-09-25 21:46:02 +0200

    komponent navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+return (
+<div className="container mt-4">
+<h1>Galeria zdjęć</h1>
+</div>
+)
+}
+export default App
